8656839cf4a62692dafe80dedc6636756effc8ad
Ahmed <halloitsmynew@gmail.com>
2025-01-09 16:55:41 +0400

    a ^ b

--- python-if_else_loops_functions/11-main.py
+++ python-if_else_loops_functions/11-main.py
@@ -6,3 +6,7 @@ print(pow(98, 2))
 print(pow(98, 0))
 print(pow(100, -2))
 print(pow(-4, 5))
+
+# a = 5 b = 3; power of a to the power b is a * b three times
+# so if for a its 2 and for b its 5 you do 2^5 multiply b times
+# x = a * 
