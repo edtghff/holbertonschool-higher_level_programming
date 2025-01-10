d7d7e29548f48a282ed12e5edddf615307338c88
Ed <edtaghiyev@gmail.com>
2025-01-10 23:40:40 +0400

    Removed files

--- python-data_structures/5-no_c.py
+++
@@ -1,2 +0,0 @@
-def no_c(my_string):
-    return ''.join([char for char in my_string if char not in ['c', 'C']])
--- python-data_structures/6-print_matrix_integer.py
+++
@@ -1,3 +0,0 @@
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(" ".join("{}".format(num) for num in row))
