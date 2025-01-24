50754f037d53f807c28bfd37e8c6c1071387bc8f
Ed <edtaghiyev@gmail.com>
2025-01-24 23:15:07 +0400

    Try-except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(min(x, len(my_list))):
-        print(my_list[i], end="")
-        count += 1
-    print()
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+
     return count
