3e7488b554a46a4629f61ba049770a06fafa6fe8
Ed <edtaghiyev@gmail.com>
2025-02-01 17:12:52 +0400

    Some fixes. I can't get understand why checker doesn't accept

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,11 +4,14 @@
 
 class Square:
     """This class defines a size."""
+
+
     def __init__(self, size=0):
         """Initialize the Square with a given size."""
+        
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        
-        self.__size = size
+        else:
+            self.__size = size
