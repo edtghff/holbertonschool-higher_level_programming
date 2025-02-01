34da29a19aadb7200dbd494708e93366d7afd7a7
Ed <edtaghiyev@gmail.com>
2025-02-01 17:09:28 +0400

    Fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-"""This module defines a Square class"""
+"""This module defines a Square class."""
 
 
 class Square:
-    """Represents a square with a given size."""
+    """Represents a square with a given size and position."""
 
     def __init__(self, size=0):
-        self.size = size
+        """Initialize the Square with a given size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     @property
     def size(self):
         """Retrieves the size of the square."""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-        """Sets the size of the value with validation"""
+        """Sets the size of the value with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
         """Calculates and returns the area of the square."""
         return self.__size ** 2
-    
-    def __str__(self):
-        """Returns a string representation of the Square instance."""
-        return f"Square(size={self.__size})"
