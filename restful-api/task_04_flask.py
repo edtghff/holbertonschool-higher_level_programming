f861955ef5e16d38259c79bc66cc40ccd9305f51
Ed <edtaghiyev@gmail.com>
2025-02-28 22:07:42 +0400

    Changed return of add_user function

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -22,7 +22,7 @@ def add_user():
     username = data['username']
 
     if username in users:
-        return jsonify({'error': 'Username already exists'}), 400
+        return jsonify({'error': 'User already exists'}), 409  # Changed message + status code
 
     user = {
         'username': username,
