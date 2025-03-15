5515eef40668bf90cb15d3c539233819fdc67a13
Ed <edtaghiyev@gmail.com>
2025-03-15 17:38:46 +0400

    Fix

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,15 +1,14 @@
 -- SQL Query
-CREATE TABLE second_table (
-    id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
     name VARCHAR(256),
     score INT
 );
 
 -- Insert records
-INSERT INTO second_table (
-    id, name, score
-) VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
