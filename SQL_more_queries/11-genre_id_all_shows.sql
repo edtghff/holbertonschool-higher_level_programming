7775cba3346b2bd5c74024c2f80b8aa88df49c44
Ed <edtaghiyev@gmail.com>
2025-03-18 18:40:33 +0400

    This was the last one

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- SQL Query
 SELECT tv_shows_title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv.shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
