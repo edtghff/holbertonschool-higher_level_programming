c74c04bdf465f15804b6a496a8d46faaf97a75ee
Ed <edtaghiyev@gmail.com>
2025-03-18 18:39:57 +0400

    Another trivial errors

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- SQL Query
-SELECT tv.shows_title, tv.show_genres.genre_id
+SELECT tv_shows_title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv.shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
