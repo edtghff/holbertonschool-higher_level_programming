978031045e7293824deb75b8d479068a30565a0b
Ed <edtaghiyev@gmail.com>
2025-03-18 17:53:19 +0400

    Changed join to inner

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- SQL Query
 SELECT id, name
 FROM cities
-JOIN states ON cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 SELECT cities.id, cities.name, states.name
 ORDER BY cities.id ASC;
