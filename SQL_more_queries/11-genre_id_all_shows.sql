52480c129bd84091073e1f0cac111e1801bd182b
Ed <edtaghiyev@gmail.com>
2025-03-18 18:38:00 +0400

    Fixed error

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,4 +2,4 @@
 SELECT tv.shows_title, tv.show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv.shows.id = tv_show_genres.show_id
-ORDER BY tv.shows_title, tv.show_genres.genre_id ASC;
+ORDER BY tv_shows_title, tv_show_genres.genre_id ASC;
