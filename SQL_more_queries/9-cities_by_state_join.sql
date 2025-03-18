35b4de8ed10d5a461a0f0feebffb3c7876964acc
Ed <edtaghiyev@gmail.com>
2025-03-18 17:54:46 +0400

    Changed the code

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- SQL Query
-SELECT id, name
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
 SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
