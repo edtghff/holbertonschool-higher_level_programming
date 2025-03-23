b13f0b34b7838fa8044e3554dd8dba61fce57713
Ed <edtaghiyev@gmail.com>
2025-03-23 19:17:58 +0400

    model state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(mysql_username, mysql_password, database_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    session.close()
