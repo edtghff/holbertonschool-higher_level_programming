adee11000dbb10a167e6f7b5572ccf468d0b80ed
Ed <edtaghiyev@gmail.com>
2025-03-30 17:30:15 +0400

    Model state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
+"""
+Defines a State class and an instance of Base for SQLAlchemy ORM.
+"""
 
-if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+Base = declarative_base()
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(mysql_username, mysql_password, database_name),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+class State(Base):
+    """
+    State class that links to the MySQL table 'states'.
+    """
+    __tablename__ = 'states'
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-
-    if state:
-        print("{}".format(state.id))
-    else:
-        print("Not found")
-
-    session.close()
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
