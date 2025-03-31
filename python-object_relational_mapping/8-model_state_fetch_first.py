9fa48c4e9b260a0bcff493e30bb6fd1099c75443
Ed <edtaghiyev@gmail.com>
2025-03-31 14:55:22 +0400

    fix

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Lists all State objects from the database hbtn_0e_6_usa."""
 
+"""Lists the first State object from the database hbtn_0e_6_usa."""
 if __name__ == "__main__":
     import sys
     from sqlalchemy import create_engine
@@ -16,13 +16,13 @@ if __name__ == "__main__":
         pool_pre_ping=True,
     )
 
-    Base.metadata.create_all(engine)
-    
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
 
     session.close()
