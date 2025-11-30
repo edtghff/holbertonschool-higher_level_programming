b126f3ef6725219793bb3ed5da1c313e8951b56c
Ahmed <87006304+edtghff@users.noreply.github.com>
2025-11-30 23:48:01 +0400

    Refactor Square class with better docstrings
    Updated the Square class to improve documentation and string representation.

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,54 +1,23 @@
 #!/usr/bin/python3
-"""
-Contains the class BaseGeometry and subclass Rectangle
-"""
+"""Module that defines a Square class inheriting from Rectangle."""
 
-
-class BaseGeometry:
-    """A class with public instance methods area and integer_validator"""
-    def area(self):
-        """raises an exception when called"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates that value is an integer greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """A representation of a rectangle"""
-    def __init__(self, width, height):
-        """instantiation of the rectangle"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """informal string representation of the rectangle"""
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """A representation of a square"""
+    """Represents a square."""
+
     def __init__(self, size):
-        """instantiation of the square"""
+        """Initialize a new Square.
+
+        Args:
+            size (int): size of the square sides.
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
-    def area(self):
-        """"returns the area of the square"""
-        return self.__size ** 2
-
     def __str__(self):
-        """informal string reepresentation of the square"""
-        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
-    
+        """Return the square description."""
+        return "[Square] {}/{}".format(self._Rectangle__width,
+                                       self._Rectangle__height)
